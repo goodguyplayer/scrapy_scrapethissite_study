4c546e180d835001d25392aeb84f48fc0deea1df
nathan <nathanbritodasilva@gmail.com>
2024-04-24 21:11:46 -0300

    Fixed way names were showing up

--- scrapethissite/scrapethissite/itemloaders.py
+++ scrapethissite/scrapethissite/itemloaders.py
@@ -1,7 +1,8 @@
-from itemloaders.processors import TakeFirst, MapCompose
+from itemloaders.processors import TakeFirst, MapCompose, Join
 from scrapy.loader import ItemLoader
 
 
 class CountryItemLoader(ItemLoader):
     default_output_processor = TakeFirst()
+    name_in = MapCompose(lambda x: x.strip(), Join(""))
     
